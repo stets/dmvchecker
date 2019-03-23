2d635238ae462a57c12230d2491ac9160ea10f53
stets <stetsblake@gmail.com>
2019-03-23 08:45:21 -0400

    initial

---
+++ main.py
@@ -0,0 +1,23 @@
+import csv
+import requests
+import words
+
+words = words.words
+
+available = []
+def plateChecker(plate):
+    base_url = 'https://services.dps.ohio.gov/BMVOnlineServices/VR/Availability/Passenger/GetAvailability?vehicleClass=&newPlate={0}&organizationCode=0'.format(plate)
+
+    r = requests.get(base_url)
+
+    if 'currently available' in r.text:
+        print('License Plate [AVAILABLE]: {0} available'.format(plate))
+    elif 'unavailable' in r.text:
+        print('License Plate [NOT AVAILABLE]: {0} unavailable'.format(plate))
+
+for word in words:
+    #print(word)
+    plateChecker(word)
+
+print(available)
+
